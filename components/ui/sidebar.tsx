2f5cd2d0a732b380c122c58acbdb631ff792bf56
trojan0x <jeffreykarau@gmail.com>
2025-08-12 20:56:12 +0200

    fix: normalize imports (remove @version suffix), add missing deps (Radix, cmdk, embla, day-picker, recharts, input-otp, react-hook-form), and lucide icon names

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import { PanelLeftIcon } from "lucide-react@0.487.0";
+import { PanelLeft } from "lucide-react";
 
 import { useIsMobile } from "./use-mobile";
 import { cn } from "./utils";
@@ -273,7 +273,7 @@ function SidebarTrigger({
       }}
       {...props}
     >
-      <PanelLeftIcon />
+      <PanelLeft />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   );
